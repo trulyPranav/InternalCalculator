b6e8286825d8c9b3985133801ea707b951d5fc94
Blackeye6941 <leok37316@gmail.com>
2024-05-13 14:46:10 +0530

    js Completed pushing for debugging

--- scripts.js
+++ scripts.js
@@ -3,3 +3,23 @@ const mark2 = document.getElementById('series2');
 const attendance = document.getElementById('attendance');
 const assignments = document.getElementById('assignments');
 const result = document.getElementById('result');
+
+function calculateTotal(event) {
+
+    event.preventDefault();
+    
+    const seriesAvg = (parseFloat(mark1.value) / 50) * 12.5 + (parseFloat(mark2.value) / 50) * 12.5;
+    let attendanceMark = 0;
+    const attendancePercentage = parseFloat(attendance.value);
+    if (attendancePercentage >= 90) {
+        attendanceMark = 10;
+    } else if (attendancePercentage >= 80 && attendancePercentage < 90) {
+        attendanceMark = 9;
+    }else {
+        attendanceMark = 8.5;
+    }
+
+    const totalMark = seriesAvg + parseFloat(assignments.value) + attendanceMark;
+    result.textContent = totalMark;
+}
+document.getElementById('marksForm').addEventListener('submit', calculateTotal);
